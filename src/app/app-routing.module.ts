82b7a23bfc0b50561dffebc0f4d41b96a5c73871
kurubusi1 <kurubusi1111@gmail.com>
2020-09-16 23:05:52 +0900

    レクチャー86

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductModule } from './product/product.module';
 
 const routes: Routes = [
-  // { path: '', component: ProductListComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
   // { path: 'detail', component: ProductDetailComponent },
 ];
 
